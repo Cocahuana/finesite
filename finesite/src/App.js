52bdd4115c66e4842e78f795c50ff47ea54877db
Cocahuana <ezequieldominguez11@gmail.com>
2022-12-07 12:03:36 -0300

    Feat: Folder structure

--- finesite/src/App.js
+++ finesite/src/App.js
@@ -1,11 +1,9 @@
-import logo from "./logo.svg";
-import "./App.css";
-
+import { Router, Route } from "react-router-dom";
 function App() {
 	return (
-		<div className='App'>
-			<h1>Finesite great Website ;D"</h1>
-		</div>
+		<>
+			<Router></Router>
+		</>
 	);
 }
 
