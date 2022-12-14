513efbcb9762556dbbefb1490f73ccd83545652d
Cocahuana <ezequieldominguez11@gmail.com>
2022-12-14 09:30:15 -0300

    Feat: add 3 types of Finesite logos

--- finesite/src/components/ui/SVG/SVGLogo.jsx
+++ finesite/src/components/ui/SVG/SVGLogo.jsx
@@ -1,6 +1,27 @@
 import React from "react";
 
 function SVGLogo(props) {
+	// None of these logos have text
+	const type = props.type ? props.type : "yellow";
+	let choosenLogo;
+	switch (type.toLowerCase()) {
+		case "yellow":
+			choosenLogo = FinesiteYellow(props);
+			break;
+		case "white":
+			choosenLogo = FinesiteWhite(props);
+			break;
+		case "black":
+			choosenLogo = FinesiteBlack(props);
+			break;
+		default:
+			choosenLogo = FinesiteYellow(props);
+	}
+	// const choosenLogo =  === "yellow" ?
+	return <>{choosenLogo}</>;
+}
+
+function FinesiteYellow(props) {
 	return (
 		<svg
 			width={props.width ? props.width : "113"}
@@ -15,6 +36,75 @@ function SVGLogo(props) {
 				stroke-width='9'
 				stroke-miterlimit='10'
 			/>
+		</svg>
+	);
+}
+
+function FinesiteWhite(props) {
+	return (
+		<svg
+			width={props.width ? props.width : "113"}
+			height={props.height ? props.height : "79"}
+			viewBox='0 0 87 61'
+			fill='none'
+			xmlns='http://www.w3.org/2000/svg'
+		>
+			<path
+				d='M5 22.1411H82M22.3276 5V22.1411M5 39.425H82M63.9717 56V39.425M5 5H82V56H5V5Z'
+				stroke='white'
+				stroke-width='9'
+				stroke-miterlimit='10'
+			/>
+		</svg>
+	);
+}
+
+function FinesiteBlack(props) {
+	return (
+		<svg
+			width={props.width ? props.width : "113"}
+			height={props.height ? props.height : "79"}
+			viewBox='0 0 315 215'
+			fill='none'
+			xmlns='http://www.w3.org/2000/svg'
+		>
+			<g clip-path='url(#clip0_27_574)'>
+				<path
+					d='M307.5 7.5H7.5V207.5H307.5V7.5Z'
+					stroke='black'
+					stroke-width='15'
+					stroke-miterlimit='10'
+				/>
+				<path
+					d='M7.5 74.72H307.5'
+					stroke='black'
+					stroke-width='15'
+					stroke-miterlimit='10'
+				/>
+				<path
+					d='M75.01 7.5V74.72'
+					stroke='black'
+					stroke-width='15'
+					stroke-miterlimit='10'
+				/>
+				<path
+					d='M7.5 142.5H307.5'
+					stroke='black'
+					stroke-width='15'
+					stroke-miterlimit='10'
+				/>
+				<path
+					d='M237.26 207.5V142.5'
+					stroke='black'
+					stroke-width='15'
+					stroke-miterlimit='10'
+				/>
+			</g>
+			<defs>
+				<clipPath id='clip0_27_574'>
+					<rect width='315' height='215' fill='white' />
+				</clipPath>
+			</defs>
 		</svg>
 	);
 }
