9454f5004dd8f704c8429e3013c725fd1949775f
Cocahuana <ezequieldominguez11@gmail.com>
2022-12-17 12:45:26 -0300

    Feat: ContentCard view for tablet

--- finesite/src/pages/LandingPage/components/ContentCard.jsx
+++ finesite/src/pages/LandingPage/components/ContentCard.jsx
@@ -3,13 +3,18 @@ import { customTheme } from "../../../assets/customTheme";
 function ContentCard(props) {
 	const { ff } = customTheme;
 	return (
-		<Flex flexDirection='column' justifyContent='center' alignItems='left'>
+		<Flex
+			flexDirection='column'
+			justifyContent='center'
+			alignItems={{ base: "center", xl: "flex-start" }}
+		>
 			<Heading
 				as='h2'
 				fontSize='2.5rem'
 				color='#F7DB5F'
 				mb='2rem'
 				mt='3rem'
+				textAlign={{ base: "center", xl: "left" }}
 			>
 				{props.title}
 			</Heading>
