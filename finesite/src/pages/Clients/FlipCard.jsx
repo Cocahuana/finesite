2c4e5d74e4aeafe4e18c7609e29b6c5123ed6c33
Cocahuana <ezequieldominguez11@gmail.com>
2023-01-06 15:52:44 -0300

    Feat: FlipCardFront and Back works fine

--- finesite/src/pages/Clients/FlipCard.jsx
+++ finesite/src/pages/Clients/FlipCard.jsx
@@ -1,5 +1,34 @@
 import "./flip.css";
 import { useState } from "react";
+import styled from "styled-components";
+
+const FlipCardContainer = styled.div`
+	background-color: transparent;
+	width: 200px;
+	height: 200px;
+	perspective: 1000px;
+	margin-left: 10px;
+`;
+
+const FlipCardFront = styled(FlipCardContainer)`
+	position: absolute;
+	width: 100%;
+	height: 100%;
+	-webkit-backface-visibility: hidden;
+	backface-visibility: hidden;
+`;
+
+const FlipCardBack = styled(FlipCardContainer)`
+	position: absolute;
+	width: 100%;
+	height: 100%;
+	-webkit-backface-visibility: hidden;
+	backface-visibility: hidden;
+	background-color: #2980b9;
+	color: white;
+	transform: rotateY(180deg);
+`;
+
 export default function FlipCard(props) {
 	const { children } = props;
 	const frontContent = children[0];
@@ -8,18 +37,29 @@ export default function FlipCard(props) {
 	const flipCard = () => {
 		setFlip(!flip);
 	};
+
 	return (
 		<button onClick={flipCard}>
-			<div
+			<FlipCardContainer
 				className={`flip-card ${
 					flip !== true ? "front-flip" : "back-flip"
 				}`}
 			>
 				<div className='flip-card-inner'>
-					<div className='flip-card-front'>{frontContent}</div>
-					<div className='flip-card-back'>{backContent}</div>
+					<FlipCardFront>{frontContent}</FlipCardFront>
+					<FlipCardBack>{backContent}</FlipCardBack>
 				</div>
-			</div>
+			</FlipCardContainer>
 		</button>
+		// 	<div
+		// 	className={`flip-card ${
+		// 		flip !== true ? "front-flip" : "back-flip"
+		// 	}`}
+		// >
+		// 	<div className='flip-card-inner'>
+		// 		<div className='flip-card-front'>{frontContent}</div>
+		// 		<div className='flip-card-back'>{backContent}</div>
+		// 	</div>
+		// </div>
 	);
 }
